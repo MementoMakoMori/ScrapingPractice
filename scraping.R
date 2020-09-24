9af1c41e217e5acfd2beeb8ca14d8c823f904178
R. Holley <36553081+MementoMakoMori@users.noreply.github.com>
2020-09-23 23:39:41 -0400

    faster scrape function; graphs -> %, grep tweak

--- scraping.R
+++ scraping.R
@@ -6,15 +6,14 @@
 ## Pff, why would I decide when I could write a program to decide for me!
 
 ## TO DO:
-## rvest function faster than Rcrawler?
 ## automate for daily/weekly reports
-## find thrid-party list of tools instead of trying to think of my own
+## find 3rd part list instead of trying to think up my own
 
-library(Rcrawler)
 library(rvest)
 library(stringi)
 library(quanteda)
 library(stopwords)
+library(plyr)
 library(ggplot2)
 library(extrafont)
 
@@ -28,43 +27,47 @@ jobIDs <- lapply(X=listHTML, FUN=stri_split_fixed, pattern="\"") %>% unlist()
 jobIDs <- jobIDs[seq.int(2, length(jobIDs), 3)]
 job_frames <- paste0("https://www.indeed.com/viewjob?viewtype=embedded&jk=", jobIDs)
 
-summ_text <- ContentScraper(job_frames, XpathPatterns = "//div[@id='jobDescriptionText']")
+summ_text <- lapply(X=job_frames, FUN=read_html) %>% lapply(FUN=html_nodes, xpath = "//div[@id='jobDescriptionText']")
 
 ## scraping is done, now for language processing
 corp <- corpus(c(unlist(summ_text)))
-
-## if just use a smple dfm and frequency plot...
-## dfm(corp) %>% textstat_frequency() %>% .[1:10,] %>% plot(x=as.factor(.$feature), y=.$frequency)
-## the biggest results are 'and' and a comma. That's boring! I'll filter the text
-
-clean_tokens <- tokens(corp, what="word", remove_punct = TRUE, padding=FALSE, verbose=TRUE)
+clean_tokens <- tokens(corp, what="word", remove_punct = TRUE, split_hyphens = TRUE, remove_symbols = TRUE, padding=FALSE, verbose=TRUE)
 clean_tokens <- tokens_remove(clean_tokens, pattern=stopwords("en"), case_insensitive=TRUE, padding=FALSE, verbose=TRUE)
-
-## dfm(clean_tokens) %>% textstat_frequency() %>% .[1:10,] %>% plot(x=as.factor(.$feature), y=.$frequency)
-
-## I know you're probably judging my ugly charts right now
-## so here's a pretty one
-freq_table <- dfm(clean_tokens) %>% textstat_frequency()
+freq_table <- dfm(clean_tokens) %>% textstat_frequency() %>% mutate(perc = 100*docfreq/length(summ_text))
 
 custom_theme1 <- theme(plot.title=element_text(family="Castellar", size=24, face="bold"), text = element_text(family="Gadugi"), plot.caption = element_text(face="italic", size=6), axis.text.x=element_text(angle=45, vjust = 0.5),
                         plot.background = element_rect(color="#F5F5F5"), panel.background=element_rect(fill="#B0E0E6"))
 
-g <- ggplot(data=freq_table[1:15,], aes(x=feature, y=frequency))               
-g + geom_col(fill="darkgoldenrod2") + custom_theme1 + labs(title="Data Science Jobs", subtitle="What are they looking for anyway?", caption="R didn't even make the top 20 :(", x=NULL)
+g <- ggplot(data=freq_table[1:15,], aes(x=feature, y=perc))               
+g + geom_bar(stat='identity',fill="darkgoldenrod2") + custom_theme1 + labs(title="Data Science Jobs", subtitle="What are they looking for anyway?", caption="R didn't even make the top 20 :(", x=NULL, y="% of Posts Containing Word")
 
 ## ...okay, maybe this isn't super helpful. I need to filter down to just what I'm looking for
 ## which may introduce a lot of human error
 ## aka I forget things
-stuff <- c("python", "SQL", "noSQL", "Hadoop", "R", "Tableau", "postgreSQL", "pyro", "pymc3", "stata", "spss", "Oracle", "Alteryx", "VBA", "excel", "matlab", "java", "weka", "gephi", "numpy", "pandas", "mysql", "aws", "qlik", "power bi", "pyspark", "scala")
+stuff <- c("python", "sql", "nosql", "hadoop", "r", "tableau", "postgresql", "pyro", "pymc3", "stata", "spss", "oracle", "alteryx", "vba", "excel", "matlab", "java", "weka", "gephi", "numpy", "pandas", "mysql", "aws", "qlik", "power bi", "pyspark", "scala", "rstudio", "access")
+
 i <- 1
 inds <- NULL
-for(i in 2:length(stuff)){
-  inds <- c(inds,grep(stuff[i], freq_table$feature, ignore.case=TRUE)[1])
+for(i in 1:length(stuff)){
+  inds <- c(inds,grep(paste0("^",stuff[i],"$"), freq_table$feature)[1])
+  i <- i+1
 }
+## make sure any tools that had zero matches are listed as 0 and not NA
+inds[which(is.na(inds))] <- 0
 
 custom_theme2 <- theme(plot.title=element_text(family="Centaur", size=20), text=element_text(family="Gadugi"), axis.text.x = element_text(angle=45, vjust=0.5),
                        plot.subtitle = element_text(face="italic"), plot.background = element_rect(fill="#F5F5F5"), panel.background=element_rect(fill="#B0E0E6"))
 
-h <- ggplot(data=freq_table[inds,], aes(x=feature, y=frequency))
-h + custom_theme2 + geom_col(fill="darkgoldenrod2") + labs(title="Popular Data Science Tools", subtitle="As Per Indeed.com Job Posts", x=NULL)
-qplot(x=feature, y=frequency, data=freq_table[inds,])
+h <- ggplot(data=freq_table[inds,], aes(x=feature, y=perc))
+h + custom_theme2 + geom_bar(stat='identity', fill="darkgoldenrod2") + labs(title="Popular Data Science Tools", subtitle="As Per Indeed.com Job Posts", x=NULL, y="% of Posts Containing Word")
+
+## speed tests
+# start <- Sys.time()
+# summ_text <- ContentScraper(job_frames, XpathPatterns = "//div[@id='jobDescriptionText']")
+# Sys.time() - start
+# Time difference of 2.432893 mins
+# 
+# st2 <- Sys.time()
+# rvest_text <- lapply(X=job_frames, FUN=read_html) %>% lapply(FUN=html_nodes, xpath = "//div[@id='jobDescriptionText']")
+# Sys.time() - st2
+# Time difference of 25.78247 secs
